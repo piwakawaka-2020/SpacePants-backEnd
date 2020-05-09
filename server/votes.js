d0f07f664c301a297847a41e8cc5610adba70e88
Daniel Windsor <dw.accounts@protonmail.com>
2020-05-09 16:48:58 +1200

    Move to majority voting

--- server/votes.js
+++ server/votes.js
@@ -11,8 +11,13 @@ function collateVotes(io, room, vote) {
   let clients = io.sockets.adapter.rooms[room]
 
   //If unanimous, returns true.  Otherwise returns false
-  if (votes[room].length === clients.length - 1) {
-    io.to(room).emit('voteResult', votes[room].every(el => el === true))
+  // if (votes[room].length === clients.length - 1) {
+  //   io.to(room).emit('voteResult', votes[room].every(el => el === true))
+  // }
+
+  if(votes[room].length === clients.length) {
+    const aye = votes[room].filter(el => el)
+    io.to(room).emit('voteResult', (aye.length > votes[room].length / 2))
   }
 }
 
